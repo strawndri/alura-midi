e5e1ad072e4e292a42abee7205d6b8da78d2529a
Andrieli <andrieliluci@gmail.com>
2023-03-14 12:37:32 -0300

    usando laços de repetição para percorrer a lista de teclas e tocar sons

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play()
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play()
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-listaDeTeclas[0].onclick = tocaSomPom
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    
+    const tecla = listaDeTeclas[i]
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+}
